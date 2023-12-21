0f098e74dd40cf313b30793cbd8fd550be1e7ec5
Midhun K M <StarkGangZ@gmail.com>
2023-12-21 19:35:21 +0530

    remove db files

--- core/sqlite/__init__.py
+++ core/sqlite/__init__.py
@@ -2,7 +2,7 @@ import sqlite3
 from typing import Union
 import time
 import random
-
+from ..utils.helpers import generate_unique_5_digit_number
 
 class SQLITEDATABASE:
     def __init__(self, client: sqlite3.Cursor, table_name: str):
@@ -11,12 +11,8 @@ class SQLITEDATABASE:
 
     def insert(self, data: Union[dict, list], list_name: str = None):
         """Inserts data into table"""
-
         orginal_key_name = None
-        if list_name:
-            tb_name = list_name + "_table"
-        else:
-            tb_name = self.table_name
+        tb_name = f"{list_name}_table" if list_name else self.table_name
         table_name = _id = None
         for key, value in data.items():
             if isinstance(value, dict):
@@ -39,13 +35,11 @@ class SQLITEDATABASE:
         col_decide = col_decide[:-2]
         id_ = generate_unique_5_digit_number()
         cmd = f"""CREATE TABLE IF NOT EXISTS {tb_name} (_id INT,{col_decide}"""
-        if list_name:
-            if (table_name or _id) is not None:
-                orginal_key_name = table_name.rsplit("_table", 1)[0]
-                cmd += f", FOREIGN KEY ({orginal_key_name}) REFERENCES {table_name}(_id)"
-        cmd += ")"
-
-        self.client.execute(cmd)
+        if list_name and (table_name or _id) is not None:
+            orginal_key_name = table_name.rsplit("_table", 1)[0]
+        if list_name and (table_name or _id) is not None:
+            orginal_key_name = table_name.rsplit("_table", 1)[0]
+            cmd += f", FOREIGN KEY ({orginal_key_name}) REFERENCES {table_name}(_id)"
         columns = ""
         for key in data.keys():
             if key == orginal_key_name:
@@ -57,14 +51,11 @@ class SQLITEDATABASE:
         for value in data.values():
             if isinstance(value, str):
                 values_ += f"'{value}', "
-            elif isinstance(value, bool):
-                values_ += f"{value}, "
-            elif isinstance(value, int):
+            elif isinstance(value, (bool, int)):
                 values_ += f"{value}, "
             elif isinstance(value, dict):
                 values_ += f"'{_id}_ft', "
-        values_ = values_[:-2]
-        if values_:
+        if values_ := values_[:-2]:
             cmd = f"INSERT INTO {tb_name} (_id, {columns}) VALUES ({id_}, {values_})"
         else:
             cmd = f"INSERT INTO {tb_name} (_id, {columns}) VALUES ({id_})"
@@ -74,35 +65,11 @@ class SQLITEDATABASE:
     
     def find(self, m_data: dict, tb_name=None):
         """Finds data in table"""
-        if tb_name:
-            tb_name = tb_name
-        else:
-            tb_name = self.table_name
+        tb_name = tb_name or self.table_name
         if m_data:
-            columns = ''
-            for key in m_data.keys():
-                if isinstance(m_data[key], dict):
-                    continue
-                columns += f"{key}, "
-            columns = columns[:-2]
-            values_ = ""
-            for value in m_data.values():
-                if isinstance(value, str):
-                    values_ += f"'{value}', "
-                if isinstance(value, dict):
-                    continue
-                elif isinstance(value, bool):
-                    values_ += f"{value}, "
-                elif isinstance(value, int):
-                    values_ += f"{value}, "
-            values_ = values_[:-2]
-            if values_:
-                cmd = f"SELECT * FROM {tb_name} WHERE {columns} = {values_}"
-            else:
-                cmd = f"SELECT * FROM {tb_name} WHERE {columns}"
+            cmd = self._pre_find_(m_data, tb_name)
         else:
             cmd = f"SELECT * FROM {tb_name}"
-
         self.client.execute(cmd)
         fetch_all = self.client.fetchall()
         self.client.execute(f"PRAGMA table_info({tb_name})")
@@ -116,35 +83,29 @@ class SQLITEDATABASE:
             if col.startswith("other_table_"):
                 child_table_name = col.rsplit("other_table_", 1)[1] + "_table"
                 orginal_key_name = child_table_name.rsplit("_table", 1)[0]
-                if orginal_key_name in m_data:
-                    child_table_data = m_data[orginal_key_name]
-                else:
-                    child_table_data = None
+                child_table_data = m_data.get(orginal_key_name)
                 n_data = self.find(child_table_data, child_table_name)
                 x_data[col] = n_data
-        if len(to_return) == 1:
-            return to_return[0]
-        return to_return
+        return to_return[0] if len(to_return) == 1 else to_return
 
-def generate_unique_5_digit_number():
-    timestamp = int(time.time() * 1000) 
-    random_suffix = random.randint(0, 99999)
-    unique_number = (timestamp % 100000) * 100000 + random_suffix
-    return unique_number
-
-
-_client = sqlite3.connect("test.db").cursor()
-client = SQLITEDATABASE(_client, "x_table")
-client.insert({"hello": "hi", "test": {"x": "y"}})
-
-_client.execute("SELECT name FROM sqlite_master WHERE type='table';")
-tables = _client.fetchall()
-
-found_data = client.find({"hello": "hi", "test": {"x": "y"}})
-print(found_data)
-
-
-# another 
-client.insert({"hello": "bye", "test": {"x": "z"}})
-found_data = client.find({"hello": "bye", "test": {"x": "z"}})
-print(found_data)
+    # TODO Rename this here and in `find`
+    def _pre_find_(self, m_data, tb_name):
+        columns = ''.join(
+            f"{key}, "
+            for key in m_data.keys()
+            if not isinstance(m_data[key], dict)
+        )
+        columns = columns[:-2]
+        values_ = ""
+        for value in m_data.values():
+            if isinstance(value, str):
+                values_ += f"'{value}', "
+            if isinstance(value, dict):
+                continue
+            elif isinstance(value, (bool, int)):
+                values_ += f"{value}, "
+        return (
+            f"SELECT * FROM {tb_name} WHERE {columns} = {values_}"
+            if (values_ := values_[:-2])
+            else f"SELECT * FROM {tb_name} WHERE {columns}"
+        )
---
+++ core/utils/helpers.py
@@ -0,0 +1,7 @@
+import time
+import random
+
+def generate_unique_5_digit_number():
+    timestamp = int(time.time() * 1000)
+    random_suffix = random.randint(0, 99999)
+    return (timestamp % 100000) * 100000 + random_suffix
