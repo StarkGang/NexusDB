cbaefd5d0f75eade63b6727a9422ab47c9140e66
Midhun K M <StarkGangZ@gmail.com>
2023-12-22 21:23:03 +0530

    find needs more

--- core/sqlite/__init__.py
+++ core/sqlite/__init__.py
@@ -63,44 +63,6 @@ class SQLITEDATABASE:
 
 
 
-    def find(self, data: dict, tb_name=None, type_of_data=None):
-        table_name = tb_name or self.table_name
-        if type_of_data == "list":
-            sql_cmd = f"SELECT * FROM {table_name}"
-            self.client.execute(sql_cmd)
-            returned_data = self.client.fetchall()
-            return [data[0] for data in returned_data]
-        if data in (None, {}):
-            sql_cmd = f"SELECT * FROM {table_name}"
-        else:
-            sql_cmd = f"SELECT * FROM {table_name} WHERE "
-            for key, value in data.items():
-                if isinstance(value, str):
-                    sql_cmd += f"{key}='{value}' AND "
-                elif isinstance(value, int):
-                    sql_cmd += f"{key}={value} AND "
-            sql_cmd = sql_cmd[:-4]
-        self.client.execute(sql_cmd)
-        returned_data = self.client.fetchall()
-        self.client.execute(f"PRAGMA table_info({table_name})")
-        table_info = self.client.fetchall()
-        column_names = [column[1] for column in table_info]
-        data_to_return = [dict(zip(column_names, data)) for data in returned_data]
-        for data in data_to_return:
-            for key, value in data.items():
-                if key.endswith("_list_table"):
-                    raw_name = key.split("_list_table")[0]
-                    find_data = data.get(raw_name, {})
-                    data[key] = self.find(find_data, key, "list")
-                elif key.endswith("_dict_table"):
-                    raw_name = key.split("_dict_table")[0]
-                    find_data = {"_id": value}
-                    if data.get(raw_name):
-                        find_data |= data.get(raw_name)
-                    data[key] = self.find(find_data, key, dict)
-        return data_to_return[0] if len(data_to_return) == 1 else data_to_return
-
-
 
 def generate_unique_5_digit_number():
     timestamp = int(time.time() * 1000)
@@ -112,6 +74,3 @@ _client = sqlite3.connect("test.db").cursor()
 client = SQLITEDATABASE(_client, "x_tble")
 client.insert({"helo": "hi", "test": {"x": "y"}, "p": [1, 2, 3]})
 client.insert({"helo": "bi", "test": {"x": "o"}, "p": [5, 2, 3]})
-
-print(client.find({"helo": "bi"}))
-print(client.find({"helo": "bi", "test": {"x": "o"}}))
