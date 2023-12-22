ee16aaebfe4dc0d2cd2dd05ed3c614ed3b640d8d
Midhun K M <StarkGangZ@gmail.com>
2023-12-22 21:02:13 +0530

    d

--- core/sqlite/__init__.py
+++ core/sqlite/__init__.py
@@ -9,104 +9,98 @@ class SQLITEDATABASE:
         self.client = client
         self.table_name = table_name
 
-    def insert(self, data: Union[dict, list], list_name: str = None):
-        """Inserts data into table"""
 
-        orginal_key_name = None
-        tb_name = f"{list_name}_table" if list_name else self.table_name
-        table_name = _id = None
-        if isinstance(data, dict):
+    def handle_insert_list(self, table_name, data_type, data, random_id, key_name):
+        self.client.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({key_name} {data_type})")
+        for value in data:
+            self.client.execute(f"INSERT INTO {table_name} VALUES ({value})")
+        return random_id, table_name
+
+
+    def insert(self, data: Union[dict, list], key_name: str = None):
+        random_id = generate_unique_5_digit_number()
+        table_name = f"{key_name}_table" if key_name else self.table_name
+        if isinstance(data, list):
+            type_of_data = "INT" if all(isinstance(x, int) for x in data) else "TEXT"
+            return self.handle_insert_list(table_name, type_of_data, data, random_id, key_name)
+        elif isinstance(data, dict):
+            return self.handle_insert_dict(table_name, data, random_id)
+
+    def handle_insert_dict(self, table_name, data, random_id):
+        self.client.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'")
+        table_exists = self.client.fetchone()
+        if not table_exists:
+            column_names = ""
             for key, value in data.items():
-                if isinstance(value, dict):
-                    table_name, _id = self.insert(value, key)
-        col_decide = ""
+                if isinstance(value, str):
+                    column_names += f"{key} TEXT, "
+                elif isinstance(value, int):
+                    column_names += f"{key} INT, "
+                elif isinstance(value, list):
+                    column_names += f"{key}_list_table TEXT, "
+                elif isinstance(value, dict):
+                    column_names += f"{key}_dict_table TEXT, "
+            self.client.execute(f"CREATE TABLE {table_name} (_id INTEGER PRIMARY KEY, {column_names[:-2]})")
+        to_insert_columns = ""
+        to_insert_values = ""
         for key, value in data.items():
             if isinstance(value, str):
-                col_decide += f"{key} VARCHAR(255), "
-            if isinstance(value, int):
-                col_decide += f"{key} INT, "
-            if isinstance(value, float):
-                col_decide += f"{key} FLOAT, "
-            if isinstance(value, bool):
-                col_decide += f"{key} BOOLEAN, "
-            if not value:
-                col_decide += f"{key} NULL, "
-            if isinstance(value, dict):
-                orginal_key_name = key.rsplit("_table", 1)[0]
-                col_decide += f"other_table_{orginal_key_name} Varchar(9), "
-        col_decide = col_decide[:-2]
-        id_ = generate_unique_5_digit_number()
-        cmd = f"""CREATE TABLE IF NOT EXISTS {tb_name} (_id INT,{col_decide})"""
-        self.client.execute(cmd)
-        columns = ""
-        for key in data.keys():
-            if key == orginal_key_name:
-                columns += f"other_table_{key}, "
-                continue
-            columns += f"{key}, "
-        columns = columns[:-2]
-        values_ = ""
-        for value in data.values():
-            if isinstance(value, str):
-                values_ += f"'{value}', "
-            elif isinstance(value, (bool, int)):
-                values_ += f"{value}, "
+                to_insert_columns += f"{key}, "
+                to_insert_values += f"'{value}', "
+            elif isinstance(value, int):
+                to_insert_columns += f"{key}, "
+                to_insert_values += f"{value}, "
+            elif isinstance(value, list):
+                generated_id, generated_table_name = self.insert(value, f"{key}_list")
+                to_insert_columns += f"{key}_list_table, "
+                to_insert_values += f"'{generated_id}', "
             elif isinstance(value, dict):
-                values_ += f"'{_id}_ft', "
-        if values_ := values_[:-2]:
-            cmd = f"INSERT INTO {tb_name} (_id, {columns}) VALUES ({id_}, {values_})"
+                generated_id, generated_table_name = self.insert(value, f"{key}_dict")
+                to_insert_columns += f"{key}_dict_table, "
+                to_insert_values += f"'{generated_id}', "
+        self.client.execute(f"INSERT INTO {table_name} (_id, {to_insert_columns[:-2]}) VALUES ({random_id}, {to_insert_values[:-2]})")
+        return random_id, table_name
+
+
+
+    def find(self, data: dict, tb_name=None, type_of_data=None):
+        table_name = tb_name or self.table_name
+        if type_of_data == "list":
+            sql_cmd = f"SELECT * FROM {table_name}"
+            self.client.execute(sql_cmd)
+            returned_data = self.client.fetchall()
+            return [data[0] for data in returned_data]
+        if data in (None, {}):
+            sql_cmd = f"SELECT * FROM {table_name}"
         else:
-            cmd = f"INSERT INTO {tb_name} (_id, {columns}) VALUES ({id_})"
+            sql_cmd = f"SELECT * FROM {table_name} WHERE "
+            for key, value in data.items():
+                if isinstance(value, str):
+                    sql_cmd += f"{key}='{value}' AND "
+                elif isinstance(value, int):
+                    sql_cmd += f"{key}={value} AND "
+            sql_cmd = sql_cmd[:-4]
+        self.client.execute(sql_cmd)
+        returned_data = self.client.fetchall()
+        self.client.execute(f"PRAGMA table_info({table_name})")
+        table_info = self.client.fetchall()
+        column_names = [column[1] for column in table_info]
+        data_to_return = [dict(zip(column_names, data)) for data in returned_data]
+        for data in data_to_return:
+            for key, value in data.items():
+                if key.endswith("_list_table"):
+                    raw_name = key.split("_list_table")[0]
+                    find_data = data.get(raw_name, {})
+                    data[key] = self.find(find_data, key, "list")
+                elif key.endswith("_dict_table"):
+                    raw_name = key.split("_dict_table")[0]
+                    find_data = {"_id": value}
+                    if data.get(raw_name):
+                        find_data |= data.get(raw_name)
+                    data[key] = self.find(find_data, key, dict)
+        return data_to_return[0] if len(data_to_return) == 1 else data_to_return
 
-        self.client.execute(cmd)
-        return (table_name or tb_name, id_)
 
-    def find(self, m_data: dict, tb_name=None):
-        """Finds data in table"""
-        tb_name = tb_name or self.table_name
-        if m_data:
-            cmd = self.pre_search(m_data, tb_name)
-        else:
-            cmd = f"SELECT * FROM {tb_name}"
-
-        self.client.execute(cmd)
-        fetch_all = self.client.fetchall()
-        self.client.execute(f"PRAGMA table_info({tb_name})")
-        columns_info = self.client.fetchall()
-        column_names = [column[1] for column in columns_info]
-        to_return = []
-        for data in fetch_all:
-            x_data = dict(zip(column_names, data))
-            to_return.append(x_data)
-        for col in column_names:
-            if col.startswith("other_table_"):
-                child_table_name = col.rsplit("other_table_", 1)[1] + "_table"
-                orginal_key_name = child_table_name.rsplit("_table", 1)[0]
-                child_table_data = m_data.get(orginal_key_name)
-                n_data = self.find(child_table_data, child_table_name)
-                x_data[col] = n_data
-        return to_return[0] if len(to_return) == 1 else to_return
-
-    def pre_search(self, m_data, tb_name):
-        columns = ''.join(
-            f"{key}, "
-            for key in m_data.keys()
-            if not isinstance(m_data[key], dict)
-        )
-        columns = columns[:-2]
-        values_ = ""
-        for value in m_data.values():
-            if isinstance(value, str):
-                values_ += f"'{value}', "
-            if isinstance(value, dict):
-                continue
-            elif isinstance(value, (bool, int)):
-                values_ += f"{value}, "
-        return (
-            f"SELECT * FROM {tb_name} WHERE {columns} = {values_}"
-            if (values_ := values_[:-2])
-            else f"SELECT * FROM {tb_name} WHERE {columns}"
-        )
 
 def generate_unique_5_digit_number():
     timestamp = int(time.time() * 1000)
@@ -116,16 +110,8 @@ def generate_unique_5_digit_number():
 
 _client = sqlite3.connect("test.db").cursor()
 client = SQLITEDATABASE(_client, "x_tble")
-client.insert({"helo": "hi", "test": {"x": "y"}})
+client.insert({"helo": "hi", "test": {"x": "y"}, "p": [1, 2, 3]})
+client.insert({"helo": "bi", "test": {"x": "o"}, "p": [5, 2, 3]})
 
-_client.execute("SELECT name FROM sqlite_master WHERE type='table';")
-tables = _client.fetchall()
-
-found_data = client.find({"helo": "hi", "test": {"x": "y"}})
-print(found_data)
-
-
-# another 
-client.insert({"helo": "bye", "test": {"x": "z"}})
-found_data = client.find({"helo": "bye", "test": {"x": "z"}})
-print(found_data)
+print(client.find({"helo": "bi"}))
+print(client.find({"helo": "bi", "test": {"x": "o"}}))
